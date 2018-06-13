8d803e9c6c26977690911867f6c2ae2d88c86323
Hydeo <p.garciapelayo@gmail.com>
2018-06-13 13:57:36 +0200

    criteria correction

--- src/main/java/hello/repository/LinkRepoImpl.java
+++ src/main/java/hello/repository/LinkRepoImpl.java
@@ -1,14 +1,18 @@
 package hello.repository;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
 import com.mongodb.client.DistinctIterable;
 import com.mongodb.client.MongoCollection;
+import org.bson.BasicBSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import hello.entity.Link;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class LinkRepoImpl implements LinkRepoCustom {
 
@@ -18,12 +22,16 @@ public class LinkRepoImpl implements LinkRepoCustom {
 
     @Override
     public ArrayList<String> getOwnerList(String owner) {
-        Criteria criteria = new Criteria();
-        criteria.where("owner").is(owner);
         Query query = new Query();
-        query.addCriteria(criteria);
-        MongoCollection mc = mongoTemplate.getCollection("link");
-        DistinctIterable<String> c = mc.distinct("listName", String.class);
+        //Criteria criteria = new Criteria();
+        //criteria.where("owner").is(owner);
+        //query.addCriteria(Criteria.where("owner").is(owner));
+        //List<Link> owner_link_list = mongoTemplate.find(query, Link.class);
+
+        BasicDBObject distinct_criteria = new BasicDBObject();
+        distinct_criteria.append("owner",owner);
+        DistinctIterable<String> c = mongoTemplate.getCollection("link").distinct("listName",distinct_criteria, String.class);
+        
         ArrayList<String> listNames = new ArrayList<>();
         c.forEach((Block<? super String>) listNames::add);
         for (String doc : c) {
